cad1e93fb2606fa1faa90c886849e5949006faeb
Thomas Hsu <bigyihsuan@hotmail.com>
2021-11-27 12:57:17 -0500

    Close #22, #19, #32: log out feature; register, log in, logout functional

--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -2,26 +2,60 @@ package com.example.gameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.api.igdb.exceptions.RequestException;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        JSONArray datas = null;
-        try {
-            datas = ParseData.getJsonFromAssets(getApplicationContext(), "Games.JSON");
-            Log.i("data", datas.getJSONObject(0).getString("name"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        setContentView(R.layout.activity_main);
+//        JSONArray datas = null;
+//        try {
+//            datas = ParseData.getJsonFromAssets(getApplicationContext(), "Games.JSON");
+//            Log.i("data", datas.getJSONObject(0).getString("name"));
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(MainActivity.this, "Error logging out", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Toast.makeText(MainActivity.this, "Logged out!", Toast.LENGTH_SHORT).show();
+                        goLoginActivity();
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
