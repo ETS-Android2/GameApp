ef55b6dbf7caedd96a124227170ea0522221897e
darkstrr <albertwang83@gmail.com>
2021-11-28 15:35:33 -0500

    "added connection and retrieval of information from IGDB API"

--- app/src/main/java/com/example/gameapp/IgdbClient.java
+++ app/src/main/java/com/example/gameapp/IgdbClient.java
@@ -1,39 +1,70 @@
 package com.example.gameapp;
 
-import android.content.Context;
 import android.util.Log;
-
-
-import com.api.igdb.apicalypse.APICalypse;
-import com.api.igdb.apicalypse.Sort;
-import com.api.igdb.exceptions.RequestException;
-import com.api.igdb.request.IGDBWrapper;
-import com.api.igdb.request.ProtoRequestKt;
-import com.api.igdb.request.TwitchAuthenticator;
-import com.api.igdb.utils.Endpoint;
-import com.api.igdb.utils.Endpoints;
-import com.api.igdb.utils.TwitchToken;
-import com.api.igdb.*;
-import com.codepath.asynchttpclient.BuildConfig;
-import com.codepath.asynchttpclient.RequestParams;
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
-import com.codepath.oauth.OAuthBaseClient;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.List;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import proto.Game;
 import proto.GameResult;
 
 public class IgdbClient{
+
+
     public static final String REST_CONSUMER_KEY = "c3vyvocsr3oyvgbip5bm3tp0mchc2w";
-    public static final String REST_CONSUMER_SECRET = "n0v4v5a34pcu9aonyniyi69dbnlxdf";
-    private String accessToken = "bearer ";
+    private static String accessToken = "Bearer jz5rzhd0u9hqj14hidasn7lvwip9r";
 
     public IgdbClient() {
 
+    }
+
+    public static JSONArray getGame() {
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api.igdb.com/v4/games")
+                .header("Client-ID", "c3vyvocsr3oyvgbip5bm3tp0mchc2w")
+                .header("Authorization", "Bearer yspmr9vt4n8zo5ujij40yq9dn22tuh")
+                .header("Accept", "application/json")
+                .body("fields name,category,age_ratings,involved_companies,genres,checksum,rating,first_release_date,cover,summary,dlcs,artworks;sort rating desc; where rating <100; limit 20;")
+                .asJson();
+
+        int stat = jsonResponse.getStatus();
+        if (stat == 200) {
+            JSONObject temp = new JSONObject(jsonResponse);
+            JSONArray game = jsonResponse.getBody().getArray();
+            Log.i("GAMES", "" + stat);
+            Log.i("GAMES", temp.toString());
+            Log.i("GAMES1", game.toString());
+            return game;
+        }
+        return null;
+
+
+    }
+    public static String getCovers(int cover) {
+        int id = cover;
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api.igdb.com/v4/covers")
+                .header("Client-ID", "c3vyvocsr3oyvgbip5bm3tp0mchc2w")
+                .header("Authorization", "Bearer yspmr9vt4n8zo5ujij40yq9dn22tuh")
+                .header("Accept", "application/json")
+                .body("fields url; where game = "+id+"; limit 20;")
+                .asJson();
+
+        int stat = jsonResponse.getStatus();
+        Log.i("GAMES", "" + stat);
+        if (stat == 200) {
+            JSONObject temp = new JSONObject(jsonResponse);
+            JSONArray game = jsonResponse.getBody().getArray();
+            String url = game.getJSONObject(0).getString("url");
+
+            Log.i("GAMES", temp.toString());
+            Log.i("GAMES1", game.toString());
+            Log.i("GAMES", url);
+            return url;
+        }
+        return "";
     }
 
 
--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,28 @@ import com.parse.ParseUser;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize and call external API
+        IgdbClient games = new IgdbClient();
+
+        //get the top rated games
+        games.getGame();
+
+        //pass in the game ids from the JSONArray from the getGame function
+        games.getCovers(153700);
+        //Log.i("TWITCH", games.getGame().get(0));
 //        JSONArray datas = null;
 //        try {
 //            datas = ParseData.getJsonFromAssets(getApplicationContext(), "app/src/main/java/com/example/gameapp/data/Games.JSON");
