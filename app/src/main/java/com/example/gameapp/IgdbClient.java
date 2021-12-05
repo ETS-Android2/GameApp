0740f2610f809cfbb154642af1768533a39c674e
darkstrr <albertwang83@gmail.com>
2021-12-05 14:54:05 -0500

    #25, #21, #18 added relevant websites and game cover

--- app/src/main/java/com/example/gameapp/IgdbClient.java
+++ app/src/main/java/com/example/gameapp/IgdbClient.java
@@ -32,7 +32,7 @@ public class IgdbClient{
             JSONArray game = jsonResponse.getBody().getArray();
             Log.i("GAMES", "" + stat);
             Log.i("GAMES", temp.toString());
-            Log.i("GAMES1", game.toString());
+            Log.i("GAMES", game.toString());
             return game;
         }
         return null;
@@ -51,17 +51,88 @@ public class IgdbClient{
         int stat = jsonResponse.getStatus();
         Log.i("GAMES", "" + stat);
         if (stat == 200) {
+            try {
             JSONObject temp = new JSONObject(jsonResponse);
             JSONArray game = jsonResponse.getBody().getArray();
             String url = game.getJSONObject(0).getString("url");
 
-            Log.i("GAMES", temp.toString());
+            Log.i("GAMES1", temp.toString());
             Log.i("GAMES1", game.toString());
-            Log.i("GAMES", url);
+            Log.i("GAMES1", url);
             return url;
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
         }
         return "";
     }
+    public JSONArray getSearch(String game) {
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api.igdb.com/v4/games")
+                .header("Client-ID", "c3vyvocsr3oyvgbip5bm3tp0mchc2w")
+                .header("Authorization", "Bearer yspmr9vt4n8zo5ujij40yq9dn22tuh")
+                .header("Accept", "application/json")
+                .body("fields name,category,age_ratings,involved_companies,genres,checksum,rating,first_release_date,cover,summary,dlcs,artworks; where name=\""+ game + "\";")
+                .asJson();
+
+        int stat = jsonResponse.getStatus();
+        Log.i("GAMES2", "" + stat);
+        if (stat == 200) {
+            JSONObject temp = new JSONObject(jsonResponse);
+            JSONArray games = jsonResponse.getBody().getArray();
+            Log.i("GAMES2", "" + stat);
+            Log.i("GAMES2", temp.toString());
+            Log.i("GAMES2", games.toString());
+            return games;
+        }
+        return null;
+    }
+    public JSONArray getGenreGame(int genreId) {
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api.igdb.com/v4/games")
+                .header("Client-ID", "c3vyvocsr3oyvgbip5bm3tp0mchc2w")
+                .header("Authorization", "Bearer yspmr9vt4n8zo5ujij40yq9dn22tuh")
+                .header("Accept", "application/json")
+                .body("fields name,category,age_ratings,involved_companies,genres,checksum,rating,first_release_date,cover,summary,dlcs,artworks;sort rating desc; where genres="+ genreId +"; limit 20;")
+                .asJson();
+
+        int stat = jsonResponse.getStatus();
+        if (stat == 200) {
+            JSONObject temp = new JSONObject(jsonResponse);
+            JSONArray game = jsonResponse.getBody().getArray();
+            Log.i("GAMES3", "" + stat);
+            Log.i("GAMES3", temp.toString());
+            Log.i("GAMES3", game.toString());
+            return game;
+        }
+        return null;
+    }
+    public JSONArray getWebsite(int cover) {
+        int id = cover;
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("https://api.igdb.com/v4/websites")
+                .header("Client-ID", "c3vyvocsr3oyvgbip5bm3tp0mchc2w")
+                .header("Authorization", "Bearer yspmr9vt4n8zo5ujij40yq9dn22tuh")
+                .header("Accept", "application/json")
+                .body("fields url; where game = "+id+"; limit 20;")
+                .asJson();
+
+        int stat = jsonResponse.getStatus();
+        Log.i("GAMES", "" + stat);
+        if (stat == 200) {
+            try {
+                JSONObject temp = new JSONObject(jsonResponse);
+                JSONArray game = jsonResponse.getBody().getArray();
+                String url = game.getJSONObject(0).getString("url");
+
+                Log.i("GAMES5", temp.toString());
+                Log.i("GAMES5", game.toString());
+                Log.i("GAMES5", url);
+                return game;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 
 
 }
